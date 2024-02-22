2382d9b2e5a588c4d3283bf10f4409275c6d1600
rajorokit <160819831+rajorokit@users.noreply.github.com>
2024-02-22 11:56:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// // Question 01
+// console.log("Hello Word");
+// // Question 02
+// var myname = "tahira";
+// var message = "is a good person";
+// console.log(myname + " " + message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+// // Question 01
+// console.log("Hello Word");
+
+
+// // Question 02
+// let myname = "tahira"
+// let message = "is a good person"
+// console.log(myname+" "+message);
